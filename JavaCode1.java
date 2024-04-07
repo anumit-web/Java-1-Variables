f37ed984de28a919f2e3ebfd9450bd12d37c693c
Anumit Jooloor <anumit@gmail.com>
2024-04-07 03:18:02 -0700

    Add files

---
+++ JavaCode1.java
@@ -0,0 +1,45 @@
+/*
+/usr/local/Cellar/openjdk/21.0.2/bin/javac JavaCode1.java
+/usr/local/Cellar/openjdk/21.0.2/bin/java JavaCode1
+*/
+
+// import
+
+public class JavaCode1 {
+
+    public static void main(String[] args) {
+        System.out.println("Java Code 1");
+        System.out.println("Java is a popular programming language.");
+
+        // print string
+        String nationnal_park_1 = "Yellowstone National Park";
+        System.out.println(nationnal_park_1);
+
+        // print numbers
+        int number_of_countries_in_the_world = 195;
+        System.out.println(number_of_countries_in_the_world);
+
+        /*
+        Java Variables = 
+        String
+        int
+        float
+        char
+        boolean
+        */
+        String largest_lake_in_the_world = "Caspian Sea";
+        int number_of_islands_in_the_world = 900000;
+        float weight_of_orca = (float) 8800.00;
+        char true_or_false = 'T';
+        boolean will_apple_stock_price_go_up_in_2024 = true;
+        System.out.println("Largest lake in the world = "+largest_lake_in_the_world);
+        System.out.println("Number of islands in the world = " + number_of_islands_in_the_world);
+        System.out.println("Weight of Orca = " + weight_of_orca);
+        System.out.println("true_or_false = " +  true_or_false);
+        System.out.println("Will Apple stock price go up in 2024 = " + will_apple_stock_price_go_up_in_2024);
+
+
+
+    }
+    
+}
